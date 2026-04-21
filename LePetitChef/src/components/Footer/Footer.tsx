ba8349ad23c5616d81a8d55d8992cedb0c310cf5
karenlldl <karenlldlira@gmail.com>
2026-04-21 10:34:23 -0300

    Created project structure

---
+++ LePetitChef/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <p>Denise Santos - RM:567559 </p>
+            <p>Henrique Bragueixe - RM:568292 </p>
+            <p>Karen Cardoso - RM:566870 </p>
+            <p>Copyright © 2025 Le Petit Chef. Todos os direitos reservados.</p>
+        </footer>
+        </>
+    )
+}
+export default Footer;
